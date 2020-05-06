325ce76546679322143aa2f16b4ea3dd53ca6281
jonndoe <qb89@yandex.ru>
2020-05-07 01:03:50 +0300

    debug false

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -27,7 +27,7 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
